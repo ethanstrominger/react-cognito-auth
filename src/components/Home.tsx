2e53ab1ddd00f8da9801833d249567014efd0bf7
Ethan Strominger <ethanstrominger2@gmail.com>
2024-12-06 11:22:01 -0500

    Remove unnecessary imports

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useUser } from '../contexts/UserContext';
 import styles from '../styles/Home.module.css';
